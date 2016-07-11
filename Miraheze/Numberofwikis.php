89f042a25e75cf7caa73f04436cfb0847d2bafb5
Reception123 <motoc.luca@gmail.com>
2016-07-11 17:55:35 +0300

    Create Numberofwikis.php

---
+++ Miraheze/Numberofwikis.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Number of wikis(SiteMatrix)
+
+class NumberOfWikis {
+	public static function assignValue( &$parser, &$cache, &$magicWordId, &$ret ) {
+		global $wgMemc;
+		if ( $magicWordId == 'NUMBEROFWIKIS' ) {
+			$key = wfMemcKey( 'metawiki', 'numberofwikis' );
+			$data = $wgMemc->get( $key );
+			if ( $data != '' ) {
+				// Cache
+				wfDebugLog(
+					'Number Of Wikis',
+					'Got the amount of wikis from memcached'
+				);
+				// return value
+				$ret = $data;
+			} else {
+				// Not cached → have to fetch it from the database
+				$dbr = wfGetDB( DB_SLAVE );
+				$res = $dbr->select(
+					'cw_wikis',
+					'COUNT(*) AS count',
+					array( 'wl_deleted' => 0 ), // ignore deleted wikis as per Jedimca0
+					__METHOD__
+				);
+				wfDebugLog( 'Number Of Wikis', 'Got the amount of wikis from DB' );
+				foreach ( $res as $row ) {
+					// Store the count in cache...
+					// (86400 = seconds in a day)
+					$wgMemc->set( $key, $row->count, 86400 );
+					// ...and return the value to the user
+					$ret = $row->count;
+				}
+			}
+		}
+		return true;
+	}
+	public static function variableIds( &$variableIds ) {
+		$variableIds[] = 'NUMBEROFWIKIS';
+		return true;
+	}
